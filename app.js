e95a8c0cc1c4df84e6db9f687e9b9e737a405b5b
Kayla Wiest <Kayla Wiest>
2016-11-28 15:34:27 -0500

    Meeting 2 files

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+//add socket.io
+app.io = require('socket.io')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/* Listen for Socket.io events */
+app.io.on('connection', function(socket){
+  //do things here when a user connects
+  console.log('a user connected');
+  app.io.emit('conn');
+
+  //do things here when a user disconnects
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+    app.io.emit('disconn');
+  });
+
+  //do things here when a message is sent
+  socket.on('new message', function(msg){
+    console.log('new message: ' + msg);
+      app.io.emit('chat message' , msg); //send the message through socket.io, caught by the script
+      });
+  });
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
